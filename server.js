80237553a07eb5e88e5bcf2a5a99ca746037a00a
Laura <lmcsay@gmail.com>
2021-02-17 11:18:32 -0600

    Feat: Connect tutorial routes to server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+require('./app/db/mongoose');
+const tutorialRouter = require('./app/routes/tutorial.routes');
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 const corsOptions = {
     origin: 'http://localhost:8081'
@@ -11,12 +14,8 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); 
+app.use(tutorialRouter);
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the app'});
-})
-
-const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
 });
